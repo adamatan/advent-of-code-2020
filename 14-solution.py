eb5d58484a08a47c8bb0349ca55e37ee8249f6a1
Adam Matan <adam@matan.name>
2020-12-14 23:48:46 +0200

    14 part 1

---
+++ 14-solution.py
@@ -0,0 +1,23 @@
+import fileinput
+import re
+
+def parse_mask(mask):
+    zero_mask = ''.join(['0' if c == '0' else '1' for c in mask])
+    one_mask = ''.join(['1' if c == '1' else '0' for c in mask])
+    return int(zero_mask,2), int(one_mask, 2)
+
+def apply_mask(mask, number):
+    return (number & mask[0]) | mask[1]
+
+mem = {}
+
+with fileinput.input() as f:
+    for line in f:
+        if 'mask' in line:
+            mask = parse_mask(line.split('=')[1].strip())
+        else:
+            address, number = re.findall(r'mem\[(\d+)\]\s+\=\s+(\d+)', line)[0]
+            address, number = int(address), int(number)
+            mem[address] = apply_mask(mask, number)
+
+print(sum(mem.values()))
