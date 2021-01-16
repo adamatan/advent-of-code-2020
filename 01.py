47e0f64b8a3135f708a71fb384867966a585aadd
Adam Matan <adam@matan.name>
2021-01-16 23:08:12 +0200

    Day 1 tests and new solutions to part 1

--- 01.py
+++ 01.py
@@ -1,3 +1,9 @@
+'''
+Solution for Advent of Code 2020, Day 1.
+https://adventofcode.com/2020/day/5
+Adam Matan <adam@matan.name>, 2021
+'''
+
 import sys
 import logging
 
@@ -6,28 +12,41 @@ def get_input(filename):
         expenses = [int(s) for s in f.readlines()]
     return expenses
 
-def part_1(filename):
-    '''
-    >>> part_1('01.txt')
+def part_1_naive(expenses):
+    '''O(N^2) soution
+    >>> part_1_naive(get_input('01.txt'))
     955584
-    >>> part_1('01-small.txt')
+    >>> part_1_naive(get_input('01-small.txt'))
     514579
     '''
-    expenses = get_input(filename)
     for i in expenses:
         for j in expenses:
             if i+j == 2020:
                 return i*j
     raise ValueError('No match found')
 
-def part_2(filename):
+def part_1_sets(expenses):
+    '''O(N^2) soution
+    >>> part_1_sets(get_input('01.txt')) == part_1_naive(get_input('01.txt'))
+    True
+    >>> part_1_sets(get_input('01-small.txt')) == part_1_naive(get_input('01-small.txt'))
+    True
     '''
-    >>> part_2('01.txt')
+    expenses = set(expenses)
+    while expenses:
+        current_expense = expenses.pop()
+        complement = 2020 - current_expense
+        if complement in expenses:
+            return current_expense * complement
+    raise ValueError('No match found')
+
+def part_2(expenses):
+    '''
+    >>> part_2(get_input('01.txt'))
     287503934
-    >>> part_2('01-small.txt')
+    >>> part_2(get_input('01-small.txt'))
     241861950
-    '''    
-    expenses = get_input(filename)
+    '''
     for i in expenses:
         for j in expenses:
             for k in expenses:
@@ -37,6 +56,8 @@ def part_2(filename):
 
 if __name__ == '__main__':
     filename = sys.argv[1] if len(sys.argv) > 1 else '01.txt'
+    file_input = get_input(filename)
     logging.basicConfig(level='WARN') # Set to INFO for debugging
-    print(part_1(filename))
-    print(part_2(filename))
+    print(part_1_naive(file_input), part_1_sets(file_input))
+    print(part_2(file_input))
+
