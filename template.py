19ae7040ff5754f74a1652af058e99ecd9af6f83
Adam Matan <adam@matan.name>
2021-01-31 01:02:00 +0200

    Add a parsing function to the template

--- template.py
+++ template.py
@@ -9,22 +9,32 @@ Adam Matan <adam@matan.name>, 2021
 import sys
 import logging
 
+def parse_input(filename):
+    '''
+    >>> 'import sys' in parse_input('template.py')
+    True
+    '''
+    with open(filename) as f:
+        return f.read()
+
 def part_1(filename):
     '''
     >>> part_1('template.txt')
     True
     '''
+    _ = parse_input(filename)
     return True
 
 def part_2(filename):
     '''
     >>> part_2('template.txt')
     True
-    '''    
+    '''
+    _ = parse_input(filename)
     return True
 
 if __name__ == '__main__':
-    filename = sys.argv[1] if len(sys.argv) > 1 else 'template.txt'
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'template.py'
     logging.basicConfig(level='WARN') # Set to INFO for debugging
     print(part_1(filename))
     print(part_2(filename))
